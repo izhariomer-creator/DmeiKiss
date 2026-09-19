0006a9396a3048f6f0e310143336a1435e9f0338
izhariomer-creator <izhariomer@gmail.com>
2026-09-19 21:19:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Dmei-Kiss service worker. Bump CACHE on every deploy so phones pick up the new build. */
-var CACHE = "dmeikiss-v17";
+var CACHE = "dmeikiss-v18";
 var SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png","./icon-32.png"];
 
 self.addEventListener("install", function(e){
